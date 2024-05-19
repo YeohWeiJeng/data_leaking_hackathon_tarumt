8fad7e529e881e3a25383a669498b7707119d08b
BlueWhale <yeohweijeng@gmail.com>
2024-05-19 22:58:02 +0800

    Improvement
    Added the Add Staff function

--- README.md
+++ README.md
@@ -22,28 +22,33 @@ This project is a simple encrypt system which can help you to encrypt or decrypt
 We're students from TARUMT Diploma in Computer Science Year 1 Semester 3. This is our first hackathon and we are excited to dive into the challenges and opportunities it presents. 
 
 ## User Guide
-1. User will required to sign in with Staff ID and password.
+1. Users will required to sign in with Staff ID and password.
 * User is unable to sign in if it is outside of the working hour (07:00 - 20:00)
 * There is only 1 staff in this system which has the Staff ID (exp: S0001) with password (exp: eJ7%y9*Lv0!mZ^3r)
 
 ![image](https://github.com/YeohWeiJeng/data_leaking_hackathon_tarumt/assets/94060398/cd6ab0e8-ce58-4ff8-b508-5ea4d8e69911)
 
-2. Users need to enter a filename (without extension) for the encryption or decryption process.
+2. Users can add more new staff.
+* The Staff ID will auto generate by the system.
+
+![image](https://github.com/YeohWeiJeng/data_leaking_hackathon_tarumt/assets/94060398/ee71efa4-b709-4b6e-bd64-9a56e9419eb8)
+
+3. Users need to enter a filename (without extension) for the encryption or decryption process.
 * If the filename that user entered is not found, an error message will be displayed and exit from the program.
 * Users can enter ‘XXX’ to exit the program.
 
 ![image](https://github.com/YeohWeiJeng/data_leaking_hackathon_tarumt/assets/94060398/84893556-9dbc-4be0-a565-ce415e93403d)
 
-3. After users enter an existing filename, the menu will be displayed and allow users to choose whether they would like to encrypt , decrypt the file or exit from the program.
+4. After users enter an existing filename, the menu will be displayed and allow users to choose whether they would like to encrypt , decrypt the file or exit from the program.
 
 ![image](https://github.com/YeohWeiJeng/data_leaking_hackathon_tarumt/assets/94060398/fc4793ac-bf27-4970-801c-318172942aa3)
 
-4. After users choose to encrypt or decrypt the file, then the program will loop back the page that lets users enter the filename. (loop back to step 3).
+5. After users choose to encrypt or decrypt the file, then the program will loop back the page that lets users enter the filename. (loop back to step 3).
 
 ![image](https://github.com/YeohWeiJeng/data_leaking_hackathon_tarumt/assets/94060398/000ae7be-015a-4dc5-b1a4-6d883b84b6a9)
 
 ![image](https://github.com/YeohWeiJeng/data_leaking_hackathon_tarumt/assets/94060398/75a8873d-f256-4798-94db-07f74e80fc7d)
 
-5. User can check the audit log inside the auditlog.txt file.
+6. User can check the audit log inside the auditlog.txt file.
 
 ![image](https://github.com/YeohWeiJeng/data_leaking_hackathon_tarumt/assets/94060398/4925d7a1-8db6-4192-aa5c-0eea17c5712b)
--- hackathon/hackathon/hackathon.c
+++ hackathon/hackathon/hackathon.c
@@ -11,6 +11,171 @@
 typedef struct {
 	char staffID[10], password[50];
 }Staff;
+Staff input;
+
+void menu(FILE* staffFile, FILE* logFile);
+int nextStaffID(FILE* staffFile);
+void addStaff(FILE* staffFile, FILE* logFile);
+void encrypt(char buffer[][MAX_LENGTH], int total, char filename[], FILE* logFile);
+void decrypt(char buffer[][MAX_LENGTH], int total, char filename[], FILE* logFile);
+void readFile(FILE* logFile);
+
+void main() {
+    SYSTEMTIME t;
+    GetLocalTime(&t);
+
+    FILE* logFile;
+    logFile = fopen("auditlog.txt", "a");
+    FILE* staffFile;
+    staffFile = fopen("staff.bin", "rb");
+    int idFound = 0, pwFound = 0;
+    if (logFile == NULL || staffFile == NULL)
+    {
+        printf("\aCan't open file\n");
+        exit(-1);
+    }
+
+    if (t.wHour < 7 || t.wHour > 20) // If out of working hour (07:00 - 20:00)
+    {
+        fprintf(logFile, "%02d-%02d-%d %02d:%02d - Someone try to login outside working hour\n", t.wDay, t.wMonth, t.wYear, t.wHour, t.wMinute);
+        printf("Sorry, the working hour is from 7:00 to 20:00. You can't access this system outside of the working hour.\n");
+        fclose(logFile);
+        exit(-1);
+    }
+
+    Staff s;
+
+    printf("Please enter your Staff ID : ");
+    rewind(stdin);
+    scanf("%s", input.staffID);
+
+    printf("Please enter your password : ");
+    int i = 0;
+    char ch;
+    while (1) {
+        ch = getch();
+        if (ch == 13) { // Enter key
+            input.password[i] = '\0';
+            break;
+        }
+        else if (ch == 8) { // Backspace key
+            if (i > 0) {
+                i--;
+                printf("\b \b");
+            }
+        }
+        else if (i < 49) { // Limit input to 49 characters
+            input.password[i++] = ch;
+            printf("*");
+        }
+    }
+
+    fread(&s, sizeof(Staff), 1, staffFile);
+    while (!feof(staffFile)) {
+        if (strcmp(s.staffID, input.staffID) == 0) {
+            if (strcmp(s.password, input.password) == 0) {
+                fprintf(logFile, "%02d-%02d-%d %02d:%02d - Login activity detected\n", t.wDay, t.wMonth, t.wYear, t.wHour, t.wMinute);
+                pwFound++;
+                menu(staffFile, logFile);
+            }
+            idFound++;
+        }
+        fread(&s, sizeof(Staff), 1, staffFile);
+    }
+    if (idFound == 0)
+    {
+        fprintf(logFile, "%02d-%02d-%d %02d:%02d - Someone entered a wrong Staff ID\n", t.wDay, t.wMonth, t.wYear, t.wHour, t.wMinute);
+        printf("\nWrong ID. Please try again later.\n");
+        fclose(logFile);
+        exit(-1);
+    }
+    if (pwFound == 0)
+    {
+        fprintf(logFile, "%02d-%02d-%d %02d:%02d - Someone entered a wrong password\n", t.wDay, t.wMonth, t.wYear, t.wHour, t.wMinute);
+        printf("\nWrong password. Please try again later.\n");
+        fclose(logFile);
+        exit(-1);
+    }
+    fclose(staffFile);
+    fclose(logFile);
+}
+
+void menu(FILE* staffFile, FILE* logFile) {
+    int choice;
+    system("cls");
+    do {
+        printf("1. Add Staff\n2. File Processing\n3. Exit\nYour Choice: ");
+        scanf("%d", &choice);
+        if (choice == 1)
+            addStaff(staffFile, logFile);
+        else if (choice == 2)
+            readFile(logFile);
+        else if (choice == 3)
+            break;
+    } while (choice < 1 || choice > 3);
+}
+
+int nextStaffID(FILE* staffFile) {
+    Staff temp;
+    int lastID = 0, id;
+
+    rewind(staffFile);
+    while (fread(&temp, sizeof(Staff), 1, staffFile) == 1) {
+        sscanf(temp.staffID, "S%d", &id);
+        if (id > lastID)
+            lastID = id;
+    }
+
+    return lastID + 1;
+}
+
+void addStaff(FILE* staffFile, FILE* logFile) {
+    SYSTEMTIME t;
+    GetLocalTime(&t);
+
+    Staff new;
+    FILE* fptr;
+    fptr = fopen("staff.bin", "ab");
+    int lastID;
+    char choice;
+    lastID = nextStaffID(staffFile);
+    sprintf(new.staffID, "S%04d", lastID);
+    printf("Staff ID: %s\n", new.staffID);
+
+    printf("Enter password: ");
+    int i = 0;
+    char ch;
+    while (1) {
+        ch = getch();
+        if (ch == 13) { // Enter key
+            new.password[i] = '\0';
+            break;
+        }
+        else if (ch == 8) { // Backspace key
+            if (i > 0) {
+                i--;
+                printf("\b \b");
+            }
+        }
+        else if (i < 49) { // Limit input to 49 characters
+            new.password[i++] = ch;
+            printf("*");
+        }
+    }
+
+    fprintf(logFile, "%02d-%02d-%d %02d:%02d - Staff %s added Staff %s\n", t.wDay, t.wMonth, t.wYear, t.wHour, t.wMinute, input.staffID, new.staffID);
+
+    fwrite(&new, sizeof(Staff), 1, fptr);
+
+    fclose(fptr);
+    printf("\nAdd more staff? (Y/N) ");
+    rewind(stdin);
+    scanf("%c", &choice);
+    if (toupper(choice) == 'Y')
+        addStaff(staffFile, logFile);
+    else
+        menu(staffFile, logFile);
+}
 
 void encrypt(char buffer[][MAX_LENGTH], int total, char filename[], FILE* logFile) {
 	SYSTEMTIME t;
@@ -107,84 +272,4 @@ void readFile(FILE* logFile)
 	} while (ch != 3);
 
 	fclose(fptr);
-}
-
-void main() {
-	SYSTEMTIME t;
-	GetLocalTime(&t);
-
-	FILE* logFile;
-	logFile = fopen("auditlog.txt", "a");
-	FILE* staffFile;
-	staffFile = fopen("staff.bin", "rb");
-	int idFound = 0, pwFound = 0;
-	if (logFile == NULL || staffFile == NULL)
-	{
-		printf("\aCan't open file\n");
-		exit(-1);
-	}
-
-	if (t.wHour < 7 || t.wHour > 20) // If out of working hour (07:00 - 20:00)
-	{
-		fprintf(logFile, "%02d-%02d-%d %02d:%02d - Someone try to login outside working hour\n", t.wDay, t.wMonth, t.wYear, t.wHour, t.wMinute);
-		printf("Sorry, the working hour is from 7:00 to 20:00. You can't access this system outside of the working hour.\n");
-		fclose(logFile);
-		exit(-1);
-	}
-
-	Staff s, input;
-
-	printf("Please enter your Staff ID : ");
-	rewind(stdin);
-	scanf("%s", input.staffID);
-
-	printf("Please enter your password : ");
-	int i = 0;
-	char ch;
-	while (1) {
-		ch = getch();
-		if (ch == 13) { // Enter key
-			input.password[i] = '\0';
-			break;
-		}
-		else if (ch == 8) { // Backspace key
-			if (i > 0) {
-				i--;
-				printf("\b \b");
-			}
-		}
-		else if (i < 49) { // Limit input to 49 characters
-			input.password[i++] = ch;
-			printf("*");
-		}
-	}
-
-	fread(&s, sizeof(Staff), 1, staffFile);
-	while (!feof(staffFile)) {
-		if (strcmp(s.staffID, input.staffID) == 0) {
-			if (strcmp(s.password, input.password) == 0) {
-				fprintf(logFile, "%02d-%02d-%d %02d:%02d - Login activity detected\n", t.wDay, t.wMonth, t.wYear, t.wHour, t.wMinute);
-				readFile(logFile);
-				pwFound++;
-			}
-			idFound++;
-		}
-		fread(&s, sizeof(Staff), 1, staffFile);
-	}
-	if (idFound == 0)
-	{
-		fprintf(logFile, "%02d-%02d-%d %02d:%02d - Someone entered a wrong Staff ID\n", t.wDay, t.wMonth, t.wYear, t.wHour, t.wMinute);
-		printf("\nWrong ID. Please try again later.\n");
-		fclose(logFile);
-		exit(-1);
-	}
-	if (pwFound == 0)
-	{
-		fprintf(logFile, "%02d-%02d-%d %02d:%02d - Someone entered a wrong password\n", t.wDay, t.wMonth, t.wYear, t.wHour, t.wMinute);
-		printf("\nWrong password. Please try again later.\n");
-		fclose(logFile);
-		exit(-1);
-	}
-
-	fclose(logFile);
 }
